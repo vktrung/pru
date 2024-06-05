bd41ba21ab744b004b72ce324e98dcffe813f0d5
vktrung <trung040102@gmail.com>
2024-06-05 23:07:40 +0700

    music background

---
+++ Assets/AudioManager.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    [SerializeField] AudioClip musicOnStart;
+
+    AudioSource audioSource;
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+    void Start()
+    {
+        Play(musicOnStart, true);
+    }
+
+    AudioClip switchTo;
+    public void Play(AudioClip music, bool interrupt = false)
+    {
+        if (interrupt = true)
+        {
+            volume = 1f;
+            audioSource.volume = volume;
+            audioSource.clip = music;
+            audioSource.Play();
+        }
+        else
+        {
+            switchTo = music;
+            StartCoroutine(SmoothSwitchMusic());
+        }
+
+    }
+    float volume;
+    [SerializeField] float timeToSwitch;
+    IEnumerator SmoothSwitchMusic()
+    {
+        volume = 1f;
+
+        while (volume > 0f)
+        {
+            volume -= Time.deltaTime / timeToSwitch;
+            if (volume < 0f)
+            {
+                volume = 0f;
+            }
+            audioSource.volume = volume;
+            yield return new WaitForEndOfFrame();
+        }
+
+        Play(switchTo, true);
+    }
+}
